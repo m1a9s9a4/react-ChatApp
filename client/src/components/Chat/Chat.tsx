dc2ec97166c01cbfbc5ea4910ace617f551aa8a4
masafumi kato <xslash1105@gmail.com>
2021-01-30 23:27:09 +0900

    [update] チャット部分の処理を変更

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -4,6 +4,8 @@ import io from 'socket.io-client';
 import './Chat.css';
 import InfoBar from '../InfoBar/InfoBar';
 import Input from '../Input/Input';
+import Messages from "../Messages/Messages";
+import {IMessage} from '../Message/Message';
 let socket;
 
 interface Props {
@@ -14,7 +16,7 @@ const Chat: React.FC<Props> = ({ location }) => {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
   const [message, setMessage] = useState('');
-  const [messages, setMessages] = useState<string[]>([]);
+  const [messages, setMessages] = useState<IMessage[]>([]);
   const ENDPOINT = 'localhost:5000';
 
   useEffect(() => {
@@ -23,7 +25,7 @@ const Chat: React.FC<Props> = ({ location }) => {
       //@ts-ignore
       withCredentials: true,
       extraHeaders: {
-        "socket-client-header": "react-ChatApp"
+        socketClientHeader: "react-ChatApp"
       }
     });
     if (typeof name === 'string') {
@@ -33,36 +35,36 @@ const Chat: React.FC<Props> = ({ location }) => {
     if (typeof room === 'string') {
       setRoom(room);
     }
-
-    socket.emit('join', {name, room}, ({err}) => {
-
+    socket.emit('join', {name, room}, (err) => {
+      if (err) {
+        console.error(err);
+      }
     });
-
-    return () => {
-      socket.emit('disconnect');
-      socket.off();
-    }
-
-  }, [ENDPOINT, location.search])
+  }, [ENDPOINT, location.search]);
 
   useEffect(() => {
-    console.log(socket);
     socket.on('message', (message) => {
-      setMessages([...messages, message])
+      setMessages([...messages, message]);
+      console.log("=================================");
+      console.log(messages);
+      console.log("=================================");
     })
-  }, [messages]);
+  }, []);
 
   const sendMessage = (event) => {
     event.preventDefault();
     if (message) {
-      socket.emit('sendMessage', message, () => setMessage(''));
+      socket.emit('sendMessage', message, () => {
+        setMessage('')
+      });
     }
   }
-console.log(messages);
+
   return (
     <div className="outerContainer">
       <div className="container">
         <InfoBar room={room} />
+        <Messages messages={messages} name={name} />
         <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
       </div>
     </div>
